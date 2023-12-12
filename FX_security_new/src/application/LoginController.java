9dd239ed1b950650270fdd4ebc0708f0a296bb99
MPMK4ever <minyoo84@naver.com>
2023-12-13 00:14:52 +1300

    Encryption is added

--- FX_security_new/src/application/LoginController.java
+++ FX_security_new/src/application/LoginController.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -8,7 +10,11 @@ import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -30,8 +36,6 @@ public class LoginController implements Initializable {
 	public void loginButtonOnAction(ActionEvent event) {
 
 		if (usernameTextField.getText().isBlank() == false && PasswordField.getText().isBlank() == false) {
-			// loginMessageLabel.setText("You tried to Login!");
-
 			validateLogin();
 		} else {
 			loginMessageLabel.setText("Please enter username and password!");
@@ -60,6 +64,9 @@ public class LoginController implements Initializable {
 				if (queryResult.getInt(1) == 1) {
 					loginMessageLabel.setText("Welcome!");
 
+					// Open the Encryption.fxml file after successful login
+					openEncryptionFXML();
+
 				} else {
 					loginMessageLabel.setText("Invalid Login. Please try again!");
 				}
@@ -70,6 +77,44 @@ public class LoginController implements Initializable {
 
 		}
 
+	}
+
+	private void openEncryptionFXML() {
+		try {
+
+			FXMLLoader loader = new FXMLLoader(
+					new File("/Users/my/git/repository3/FX_security_new/src/application/Encryption.fxml").toURI()
+							.toURL());
+
+			// FXMLLoader loader = new FXMLLoader();
+			// loader.setLocation(getClass().getResource("Encryption.fxml"));
+			// System.out.println(this.getClass().getResource("Encryption.fxml"));
+			Parent root = loader.load();
+			// EncryptionController controller = loader.getController();
+
+			Stage stage = new Stage();
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.setTitle("Data Encryption");
+			stage.setMinHeight(400);
+			stage.setMinWidth(520);
+			stage.show();
+
+			// Close the login window
+			cancelButtonOnAction(null);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			showAlert("Error", "Error loading Encryption.fxml", "An error occurred while opening the Encryption view.");
+		}
+	}
+
+	private void showAlert(String title, String header, String content) {
+		Alert alert = new Alert(Alert.AlertType.ERROR);
+		alert.setTitle(title);
+		alert.setHeaderText(header);
+		alert.setContentText(content);
+		alert.showAndWait();
 	}
 
 	@Override
